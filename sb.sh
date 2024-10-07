e0057aa012a644e1c59f97a40c38b70111b033f0
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2024-10-07 13:53:57 +0800

    Update sb.sh

--- sb.sh
+++ sb.sh
@@ -4271,6 +4271,7 @@ rm /tmp/crontab.tmp
 }
 
 lnsb(){
+rm -rf /usr/bin/sb
 curl -L -o /usr/bin/sb -# --retry 2 --insecure https://raw.githubusercontent.com/yonggekkk/sing-box-yg/main/sb.sh
 chmod +x /usr/bin/sb
 }
