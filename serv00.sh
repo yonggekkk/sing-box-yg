4e4a75653f0b28370b8758d8c8e4d4cdc7643fa8
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2024-12-30 17:07:04 +0800

    Update serv00.sh

--- serv00.sh
+++ serv00.sh
@@ -466,7 +466,7 @@ get_links(){
 argodomain=$(get_argodomain)
 echo -e "\e[1;32mArgo域名:\e[1;35m${argodomain}\e[0m\n"
 green "安装进程保活"
-curl -sSL https://raw.githubusercontent.com/yonggekkk/Cloudflare_vless_trojan/main/serv00keep.sh -o serv00keep.sh && chmod +x serv00keep.sh
+curl -sSL https://raw.githubusercontent.com/yonggekkk/sing-box-yg/main/serv00keep.sh -o serv00keep.sh && chmod +x serv00keep.sh
 sed -i '' -e "18s|''|'$UUID'|" serv00keep.sh
 sed -i '' -e "21s|''|'$vless_port'|" serv00keep.sh
 sed -i '' -e "22s|''|'$vmess_port'|" serv00keep.sh
