3125ee124c49d8ced173c3b8a54aa6f94fc51b1a
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-01-10 21:27:48 +0800

    Update serv00keep.sh

--- serv00keep.sh
+++ serv00keep.sh
@@ -25,7 +25,7 @@ if [[ "$reset" =~ ^[Yy]$ ]]; then
 crontab -l | grep -v "serv00keep" >rmcron
 crontab rmcron >/dev/null 2>&1
 rm rmcron
-ps aux | grep $(whoami) | grep -v "sshd\|bash\|grep" | awk '{print $2}' | xargs -r kill -9 > /dev/null 2>&1
+bash -c 'ps aux | grep $(whoami) | grep -v "sshd\|bash\|grep" | awk "{print \$2}" | xargs -r kill -9 >/dev/null 2>&1' >/dev/null 2>&1
 find ~ -type f -exec chmod 644 {} \; 2>/dev/null
 find ~ -type d -exec chmod 755 {} \; 2>/dev/null
 find ~ -type f -exec rm -f {} \; 2>/dev/null
