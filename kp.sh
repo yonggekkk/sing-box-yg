42d38f763c09a36edce03e1259ddb794353c1ad6
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-02-14 16:28:06 +0800

    Update kp.sh

--- kp.sh
+++ kp.sh
@@ -53,7 +53,7 @@ echo "甬哥Github项目  ：github.com/yonggekkk"
 echo "甬哥Blogger博客 ：ygkkk.blogspot.com"
 echo "甬哥YouTube频道 ：www.youtube.com/@ygkkk"
 echo "自动远程部署Serv00三合一协议脚本【VPS+软路由】"
-echo "版本：V25.2.13"
+echo "版本：V25.2.14"
 echo "*****************************************************"
 echo "*****************************************************"
               count=0  
