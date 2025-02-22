76990cd6a8f77b5dbe3cf4d8c7d640a3e806ae81
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-02-22 12:40:34 +0800

    Update serv00.sh

--- serv00.sh
+++ serv00.sh
@@ -1331,7 +1331,7 @@ dig @8.8.8.8 +time=5 +short "web$nb.serv00.com" >> $WORKDIR/hy2ip.txt
 dig @8.8.8.8 +time=5 +short "$HOSTNAME" >> $WORKDIR/hy2ip.txt
 dig @8.8.8.8 +time=5 +short "cache$nb.serv00.com" >> $WORKDIR/hy2ip.txt
 for host in "${ym[@]}"; do
-response=$(curl -sL --connect-timeout 5 --max-time 7 "https://ss.serv0.us.kg/api/getip?host=$host")
+response=$(curl -sL --connect-timeout 5 --max-time 7 "https://ss.fkj.pp.ua/api/getip?host=$host")
 if [[ "$response" =~ ^$|unknown|not|error ]]; then
 dig @8.8.8.8 +time=5 +short $host >> $WORKDIR/ip.txt
 sleep 1 
