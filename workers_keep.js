b03f493d5e066867243eb0be063e4ddee546ef35
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-02-23 17:02:35 +0800

    Update workers_keep.js

--- workers_keep.js
+++ workers_keep.js
@@ -1,8 +1,8 @@
 addEventListener('scheduled', event => event.waitUntil(handleScheduled()));
 // 配合甬哥的bash <(curl -Ls https://raw.githubusercontent.com/yonggekkk/sing-box-yg/main/serv00.sh)脚本，生成保活网页
-// 每个保活网页之间空格，网页前带http://
+// 每个保活网页之间用空格或者，或者,间隔开，网页前带http://
 const urlString = 'http://保活网页1 http://保活网页2 http://保活网页3';
-const urls = urlString.split(' ');
+const urls = urlString.split(/[\s,，]+/);
 const TIMEOUT = 5000;
 async function fetchWithTimeout(url) {
   const controller = new AbortController();
