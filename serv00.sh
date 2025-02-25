4e03c483162c11c1fce10c2bf50c71a84c32dc50
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-02-25 11:18:25 +0800

    Update serv00.sh

--- serv00.sh
+++ serv00.sh
@@ -153,7 +153,7 @@ if [[ $tcp_ports -ne 2 || $udp_ports -ne 1 ]]; then
     #green "端口已调整完成,将断开ssh连接,请重新连接shh重新执行脚本"
     #devil binexec on >/dev/null 2>&1
     #kill -9 $(ps -o ppid= -p $$) >/dev/null 2>&1
-    #sleep 2
+    sleep 3
 else
     tcp_ports=$(echo "$port_list" | awk '/tcp/ {print $1}')
     tcp_port1=$(echo "$tcp_ports" | sed -n '1p')
