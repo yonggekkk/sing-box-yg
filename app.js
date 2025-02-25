18d25762259dab8c6109f3246fc7d1e262dd44df
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-02-25 21:40:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.get("/rp", (req, res) => {
     const changeportCommands = `
 USERNAME=$(whoami | tr '[:upper:]' '[:lower:]')
 WORKDIR="/home/\${USERNAME}/domains/\${USERNAME}.serv00.net/logs"
-portlist="\$(devil port list | grep -E '^[0-9]+[[:space:]]+[a-zA-Z]+' | sed 's/^[[:space:]]*//')"
+portlist=\$(devil port list | grep -E '^[0-9]+[[:space:]]+[a-zA-Z]+' | sed 's/^[[:space:]]*//')
 if [[ -z "\$portlist" ]]; then
 echo "无端口"
 else
@@ -50,7 +50,7 @@ port_type=\$(echo "\$line" | awk '{print $2}')
 echo "删除端口 \$port (\$port_type)"
 devil port del "\$port_type" "\$port"
 done <<< "\$portlist"
-fi
+fi    
 port_list=\$(devil port list)
 tcp_ports=\$(echo "\$port_list" | grep -c "tcp")
 udp_ports=\$(echo "\$port_list" | grep -c "udp")
@@ -74,7 +74,7 @@ if [[ \$tcp_ports -ne 2 || \$udp_ports -ne 1 ]]; then
         tcp_ports_to_add=\$((2 - tcp_ports))
         tcp_ports_added=0
         while [[ \$tcp_ports_added -lt \$tcp_ports_to_add ]]; do
-            tcp_port=\$(shuf -i 10000-65535 -n 1)
+            tcp_port=\$(shuf -i 10000-65535 -n 1) 
             result=\$(devil port add tcp \$tcp_port 2>&1)
             if [[ \$result == *"succesfully"* ]]; then
                 echo "已添加TCP端口: \$tcp_port"
@@ -91,7 +91,7 @@ if [[ \$tcp_ports -ne 2 || \$udp_ports -ne 1 ]]; then
     fi
     if [[ \$udp_ports -lt 1 ]]; then
         while true; do
-            udp_port=\$(shuf -i 10000-65535 -n 1)
+            udp_port=\$(shuf -i 10000-65535 -n 1) 
             result=\$(devil port add udp \$udp_port 2>&1)
             if [[ \$result == *"succesfully"* ]]; then
                 echo "已添加UDP端口: \$udp_port"
@@ -130,7 +130,7 @@ sed -i '' -e "17s|\$vlp|'\$vless_port'|" serv00keep.sh
 sed -i '' -e "18s|\$vmp|'\$vmess_port'|" serv00keep.sh
 sed -i '' -e "19s|\$hyp|'\$hy2_port'|" serv00keep.sh
 bash -c 'ps aux | grep \$(whoami) | grep -v "sshd\|bash\|grep" | awk "{print \$2}" | xargs -r kill -9 >/dev/null 2>&1' >/dev/null 2>&1
-(cd ~ && bash serv00keep.sh >/dev/null 2>&1) &
+(cd ~ && bash serv00keep.sh >/dev/null 2>&1) & 
 sleep 5
 echo "端口替换完成！"
 ps aux | grep '[r]un -c con' > /dev/null && echo "主进程启动成功，单节点用户修改下客户端三协议端口，订阅链接用户更新下订阅即可" || echo "Sing-box主进程启动失败，再次重置端口或者多刷几次保活网页，可能会自动恢复"
