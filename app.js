0a5405c08c7a9207eefbea13eeb19faa7fb29de7
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-03-28 10:16:16 +0800

    Update app.js

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.get("/list/key", (req, res) => {
 app.use((req, res) => {
     res.status(404).send('请在浏览器地址：http://where.name.serv00.net 后面加三种路径功能：/up是保活，/re是重启，/rp是重置节点端口，/list/你的uuid 是节点及订阅信息');
 });
-setInterval(runCustomCommand, 30 * 60 * 1000);
+setInterval(runCustomCommand, (2 * 60 + 15) * 60 * 1000);
 app.listen(3000, () => {
     console.log("服务器运行在端口 3000");
     runCustomCommand();
