cc7d2c428425b4f893de8e802ba9f53a074c19d2
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-08-01 08:37:09 +0800

    Update sb.sh

--- sb.sh
+++ sb.sh
@@ -156,13 +156,16 @@ v4orv6(){
 if [ -z $(curl -s4m5 icanhazip.com -k) ]; then
 echo
 red "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-yellow "检测到 纯IPV6 VPS，添加DNS64"
+yellow "检测到 纯IPV6 VPS，添加NAT64"
 echo -e "nameserver 2a00:1098:2b::1\nnameserver 2a00:1098:2c::1\nnameserver 2a01:4f8:c2c:123f::1" > /etc/resolv.conf
-endip=2606:4700:d0::a29f:c101
 ipv=prefer_ipv6
 else
-endip=162.159.192.1
 ipv=prefer_ipv4
+fi
+if [ -n $(curl -s6m5 icanhazip.com -k) ]; then
+endip=2606:4700:d0::a29f:c101
+else
+endip=162.159.192.1
 fi
 }
 warpcheck
