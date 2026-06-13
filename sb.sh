cf43e1065f9e4c9642009f922b87c26a6dfca753
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2026-06-13 12:23:25 +0800

    Update sb.sh

--- sb.sh
+++ sb.sh
@@ -1540,10 +1540,6 @@ dns:
   proxy-server-nameserver:
     - "https://223.5.5.5/dns-query"
     - "https://doh.pub/dns-query"
-nameserver-policy:
-  "geosite:cn":
-     - "https://223.5.5.5/dns-query"
-     - "https://doh.pub/dns-query"
 
 proxies:
 - name: vless-reality-vision-$hostname               
